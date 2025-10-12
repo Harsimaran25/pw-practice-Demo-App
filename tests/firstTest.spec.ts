0ac5eb08f650d35c2f1d5f8943fde330cb0b3ce4
Hars <harsimaran.singh25@outlook.com>
2025-10-12 12:26:50 +1100

    added files

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,15 @@
+import {test} from '@playwright/test';
+
+
+// to group test we can use test.describe and then the tests can sit inside it
+
+test.describe('test suite 1', ()=>{
+
+
+test('first test', async({page})=>{
+
+
+})
+
+
+})
